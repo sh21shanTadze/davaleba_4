812c24d91d4ff644f0bddd782706955dbabbb205
Shavleg <shavleg.shantadze.1@btu.edu.ge>
2021-06-17 17:09:20 +0400

    finish

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Users from './component/Users';
 import Home from './component/Home';
 import About from './component/About';
-import Users from './component/Users';
 import ErrorBoundary from './component/ErrorBoundary';
 import ClickConter from './component/ClickCounter';
 import HoverConuter from './component/HoverCounter';
@@ -17,13 +17,13 @@ function App() {
                 <nav>
                     <ul>
                         <li>
+                            <Link to="/Users">Users</Link>
+                        </li>
+                        <li>
                             <Link to="/">Home</Link>
                         </li>
                         <li>
                             <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/Users">Users</Link>
                         </li>
                     </ul>
                 </nav>
